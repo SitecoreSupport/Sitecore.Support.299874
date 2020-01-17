cd79e52aaf79ec981c69b20d499c1b261b5e3b74
Ielyzaveta Kalinchuk <ielyzaveta.kalinchuk@sitecore.com>
2020-01-17 09:39:31 +0200

    Fix issue when LinkProvider.ResolveTargetSite(Item item) never resolves the site using the siteResolvingTable

---
+++ src/Sitecore.Support.299874/Links/LinkProvider.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Links;
+
+namespace Sitecore.Support.Links
+{
+    public class LinkProvider : Sitecore.Links.LinkProvider
+    {
+        public override string GetItemUrl(Item item, UrlOptions options)
+        {
+            string itemUrl;
+            options.SiteResolving = Sitecore.Configuration.Settings.Rendering.SiteResolving;
+            itemUrl = base.GetItemUrl(item, options);
+            return itemUrl;
+        }
+    }
+}
---
+++ src/Sitecore.Support.299874/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.299874")]
+[assembly: AssemblyProduct("Sitecore.Support.299874")]
+[assembly: ComVisible(false)]
